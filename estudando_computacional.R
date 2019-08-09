9fd963424180d8db9ae6e4acd99c9b8298a1dbeb
conexaomundom <48895083+conexaomundom@users.noreply.github.com>
2019-08-09 11:02:18 -0300

    Até o início de dot-dot-dot

--- estudando_computacional.R
+++ estudando_computacional.R
@@ -1,3 +1,5 @@
+install.packages("magrittr")
+
 # Passando atributo 
 x <- 1L:10L
 
@@ -32,5 +34,171 @@ f <- function(x){
 attr(f, "desc") <- "retorna -, 0 ou +, a depender do valor passado à x"
 attr(f, "desc")
 
-# A função body 
+# A função body() permite-nos acessar o corpo de uma função (codg da funcao)
+# porém só irá visualizar as partes do código que foram implementadas
+# estritamente em R. 
+
+body(ls)
+body(sum)
+
+# POdemos também paddar uma função como argumento à outra funçaõ
+# f() e g(), em que passamos f() como argumento à funcao g()
+
+# as funções não necessariamente necessitam ter argumentos
+f <- function(){
+  
+  "olá"
+}
+
+g <- function(func){
+ 
+  paste(func, "mundo", sep = " ")
+   
+}
+
+g(func = f())
+
+# Compondo as funções nchar(), g() e f()
+# A funcao nchar() retorna a quantidade de characteres
+
+nchar(g(func = f()))
+
+# Também podemos não nomear uma funcao, quando temos uma função curta 
+# que são passadas como argumento à outras funcoes 
+# Uma funcao anonima passada como argumento à funcao integrate()
+integrate (function(x) x ^ 2, lower = 0, upper = 2)$value
+
+# Po demos também compor funções utilizando o operador %>%
+# Como: 
+library(magrittr)
+f %>% g %>% nchar # ctrl + shift + m 
+
+
+# integrate(function(x))
+(function(x) x^2) %>%  integrate(lower = 0, upper = 2) %$% value
+# operadores pipes não deixam o codigo mais eficiente, nem menos
+# eficientes.
+
+# O escopo é o  ue delimita a relação de objetos e expressões, ou seja,
+# em que partes do código um ou mais objetos são reconhecidos por uma dada
+# dada expressão ou conjunto de expressões. Na maioria das linguagens 
+# de programação o escopo lexico, também chamado de escopo estático, 
+# uma vez que podem ser delimitados estatisticamente, antes da execução 
+# do programa por meio da sintaxe da linguagem, ou seja, por meio da 
+# estrutura léxica da linguagem.
+
+# R tem escopo léxico.
+
+x <- "fora"
+
+f <- function( ){
+  
+  x <- "dentro"
+  x
+}
+f()
+
+
+x <- "fora"
+f <- function(){
+  x
+}
+
+f()
+
+#####
+
+x <- "estou fora de f"
+
+f <- function(){
+  g <- function(){
+    x
+  }
+  list(g = g(), x = x)
+}
+f()
+
+###
+
+x <- 0
+
+f <- function(){
+  
+  x <- x + 1
+  x
+  
+}
+
+f()
+
+
+# mas se usar atribuição profunda
+# <<-
+x <- 0
+
+f <- function(){  x <<- x + 1;  x}
+f()
+
+# OBservando 
+x <- 0
+f <- function(){
+  x = 7
+  x <<- x + 1
+  x
+}
+
+f()
+x
+
+####
+
+f <- function(){
+  2 -> x
+  x * 2 ->> x
+  x
+}
+f(); x
+
+# Lazi evaluation -----------------------------------------------------------------
+# (promises) uma estrutura de dados
+# Promises possuem um ambiente em que uma funcao é avaliada/invocada
+# Uma expressão valida passada para um argumento de uma funcao
+# um valor, resultado da avaliação de uma expressão em um ambiente específico.
+
+# Em R os argumentos são avaliados de forma preguiçosa, são apenas
+# promessas, ou seja, só serão acvaliados caso seja necessário usar 
+# esse arguneto, se não, o parametro poderá eventualmente nunca seravaliados
+# exemplo
+
+f <- function(x){
+  
+  y <- 1
+  y
+  
+}
+f()
+
+
+# varargs:... (dot-dot-dot) -----------------------------------------------
+
+set.seed(0)
+
+data <-rnorm(n = 1000000, mean = 0, sd = 1)
+myhist <- function(x = data, ...){
+  # Perceba o uso de ... em hist():
+  
+  result <- hist(x, ...)
+  list(n = length(result$counts), counts = result$counts)
+  
+}
+
+# A funcao muhist() não possuia a definição dos argumentos col, 
+# main, ylab ee border
+
+myhist(x = data, col = rgb(1,0.9, 0.8), main = "", ylab = " freq",
+       border = NA)
+
+
+
+
 
